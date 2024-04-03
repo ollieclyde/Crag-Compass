0744e37664118fe5922569fd74e435abc3171b6d
Oliver Clyde <ollieclyde@hotmail.co.uk>
2024-04-03 17:25:50 +0100

    repair issues on production for leaflet marker

--- client/src/components/subcomponents/leaflet-map-component.tsx
+++ client/src/components/subcomponents/leaflet-map-component.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from 'leaflet';
+import { FaMapMarkerAlt } from 'react-icons/fa';
 import "leaflet/dist/leaflet.css";
+import { renderToStaticMarkup } from 'react-dom/server';
+
 
 interface CragMapProps {
   osy: string;
@@ -10,6 +14,13 @@ interface CragMapProps {
 const CragMap: React.FC<CragMapProps> = ({ osy, osx }) => {
   const latitude = parseFloat(osy);
   const longitude = parseFloat(osx);
+
+  const iconMarkup = renderToStaticMarkup(<FaMapMarkerAlt style={{ fontSize: '24px', color: 'red' }} />);
+  const customMarkerIcon = L.divIcon({
+    html: iconMarkup,
+    className: 'my-custom-icon',
+    iconSize: L.point(30, 30),
+  });
 
   return (
     <MapContainer
@@ -21,10 +32,13 @@ const CragMap: React.FC<CragMapProps> = ({ osy, osx }) => {
         attribution=""
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[latitude, longitude]}>
+      <Marker
+        position={[latitude, longitude]}
+        icon={customMarkerIcon} 
+      >
         <Popup>
           <a
-            // href={`https://www.google.com/maps?q=${latitude},${longitude}`}
+            href={`https://www.google.com/maps?q=${latitude},${longitude}`}
             target="_blank"
             rel="noopener noreferrer"
           >
